5799112e57254b5254dff92f94746124193d157c
ARYVIKING <abdurrahmanyasin999@gmail.com>
2025-10-21 15:59:23 +0600

    Create Age Detector with SwitchCase.c

---
+++ Age Detector with SwitchCase.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (void)
+{
+
+    int age;
+    printf("Enter value for age: ");
+    scanf("%d", &age);
+
+    switch(age>=18)
+    {
+      case 0 :
+        printf("\nYou are a minor!\n");
+        break;
+      case 1 :
+        printf("\nYou are a proper adult\n");
+        break;
+    }
+
+    return 0 ;
+}
