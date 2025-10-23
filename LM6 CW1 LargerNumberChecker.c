ac58dd51fb7e8a2c109e497e34aed8e06a7e23f6
ARYVIKING <abdurrahmanyasin999@gmail.com>
2025-10-23 19:56:05 +0600

    Update LM6 CW1 LargerNumberChecker.c

--- LM6 CW1 LargerNumberChecker.c
+++ LM6 CW1 LargerNumberChecker.c
@@ -1,32 +1,99 @@
 /*A program that takes two numbers as input and prints the larger number*/
 #include <stdio.h>
 
+int LeapDetect(int year); // Leap year detector function
+int factorial(int n);     // Factorial detector function
+int LargerNum (int, int, int); // Larger number detector function
 int main (void)
 {
-
     //Declaring Variables
-    int num1, num2 ;
+    int choice, year, leap, num, num1, num2, num3;
 
-    //Taking Inputs
-    printf("Enter 1st Number: ");
-    scanf("%d", &num1);
+    //Menu
+    printf("1)Check Leap Year\n");
+    printf("2)Calculate Factorial\n");
+    printf("3)Find the Largest Number of three\n");
+    printf("4)Exit Program\n");
+    printf("\nEnter choice ");
+    scanf("%d", &choice);
 
-    printf("\nEnter 2nd Number: ");
-    scanf("%d", &num2);
-
-    //Checking Condition
-    if (num1 > num2)
+    switch(choice)
     {
-        printf("\nThe Larger Number is %d\n", num1);
+    case 1: // Leap year detect
+    printf("\nEnter the Year: ");
+    scanf("%d", &year);
+    leap = LeapDetect(year);
+    break;
+
+    case 2: // Factorial Calculate
+    printf("Enter a positive Integer: ");
+    scanf("%d", &num);
+    printf("\nFactorial of %d = %d\n", num, factorial(num)/*Function Call*/);
+    break;
+
+    case 3: //Larger number detect
+    return LargerNum (num1,num2,num3);
+    break;
+
+    case 4: // Exit Program
+        printf("\nPress Enter to confirm exit\n");
+    break;
     }
-    else if (num1 < num2)
+    return 0 ;
+}
+
+
+
+// 1)Leap year detector function 
+int LeapDetect(int year)
+{
+    if (( year % 400 == 0  ) || (year % 100  && year % 4 == 0 ))
     {
-        printf("\nThe Larger Number is %d\n", num2);
+        printf("\nIt is  a leap year\n");
     }
     else
     {
-        printf("Both Numbers are Equal\n Try again..\n");
+        printf("\nIt is not a leap year\n");
     }
-    
-    return 0 ;
+}
+
+
+// 2) Factorial calculator function
+int factorial(int n)
+ {
+    if (n)
+     {
+         return n * factorial(n -1);
+     }
+     else
+         return 1;
+ }
+
+
+
+// 3) Larger number of 3 detector
+int LargerNum (int num1, int num2, int num3)
+ {
+    printf("\nEnter Three Different Integers \n Enter 1st Number: ");
+    scanf("%d", &num1);
+    printf("\n Enter 2nd Number: ");
+    scanf("%d", &num2);
+    printf("\n Enter 3rd Number: ");
+    scanf("%d", &num3);
+
+ if (num1 > num2)
+    {
+        if(num1 > num3)
+            printf(" %d is the largest number", num1);
+        else
+            printf(" %d is the largest number", num3);
+    }
+    else
+    {
+        if(num2 > num3)
+            printf(" %d is the largest number", num2);
+        else
+            printf(" %d is the largest number", num3);
+    }
+
 }
