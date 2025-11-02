2539c35c3115c9ee05b7d0b0b78f4a4ef7bd3f6b
ARYVIKING <abdurrahmanyasin999@gmail.com>
2025-11-02 16:01:32 +0600

    Update LM8 CW3 Even Number Detector between two numbers.c

--- LM8 CW3 Even Number Detector between two numbers.c
+++ LM8 CW3 Even Number Detector between two numbers.c
@@ -1,23 +1,22 @@
 /*A program to detect even numbers between to numbers and print those in descending order*/
 #include <stdio.h>
 
-int Num(int m, int n); // Function Call
+int PrintEven(int m, int n); // Function Call
 int main (void)
 {
     int m, n;  // Declaring Variables
-    printf("\nEnter value of m & n: ");
-    scanf("%d%d", &m, &n);
 
-
-    Num(m,n); // Function Call
+    PrintEven(m,n); // Function Call
 
     return 0 ;
 }
 
 
 // Function Definition
-int Num(int m, int n)
+int PrintEven(int m, int n)
 {
+    printf("\nEnter value of m & n: ");
+    scanf("%d%d", &m, &n);
     for(int i = n; i >= m; i--)
     {
         if(i%2 == 0)
