f998db35428b71d865aafd5809de4c2ec15f1a0e
ARYVIKING <abdurrahmanyasin999@gmail.com>
2025-11-17 16:30:31 +0600

    Update and rename LM10 CW1 NestedLoop.c to LM10 CW1 HillPatternAlternateElements.c

--- LM10 CW1 HillPatternAlternateElements.c
+++ LM10 CW1 HillPatternAlternateElements.c
@@ -1,32 +1,29 @@
+/*A program to print alternate elements in a hill pattern*/
 #include <stdio.h>
 
-int Forloop (void);  // Function 
-int main(void)
-
+int hill(int n);
+int main (void)
 {
+    int num;
+    printf("\nEnter number of rows: ");
+    scanf("%d", &num);
 
-    Forloop();
+    hill(num);
+
     return 0;
 }
 
-int Forloop (void)
+int hill(int n)
 {
-    int n;
-    printf("Enter value of n: ");
-    scanf("%d", &n);
     for(int i = 1; i <= n; i++)
     {
         for(int j = 1; j <= i; j++)
         {
             if(j%2)
-            {
                 printf(" 1 ");
-            }
             else
                 printf(" 0 ");
-        }
-        printf("\n");
+        }printf("\n");
     }
-    return 0 ;
-}
 
+}
