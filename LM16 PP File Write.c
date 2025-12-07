193aa6d53f77881add88cc9828d7dcf989011594
ARYVIKING <abdurrahmanyasin999@gmail.com>
2025-12-07 15:33:53 +0600

    Create LM16 PP File Write.c

---
+++ LM16 PP File Write.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Student
+{
+    char name[50];
+    int roll;
+    double cgpa;
+}typedef student;
+
+int main(void)
+{
+    FILE *fp;
+
+    fp = fopen("student_info2.txt", "w");
+
+    Student students[3];
+    char name[50];
+    int roll;
+    double cgpa;
+
+    for(int i = 0; i < 3; i++)
+    {
+        printf("Enter your student %d's name: \n", i+1);
+        gets(name);
+        printf("Enter roll: \n");
+        scanf("%d", &roll);
+        printf("Enter CGPA: \n");
+        scanf("%lf", &cgpa);
+
+        getchar();
+        fprintf(fp,"%s %d %.2f\n", name, roll, cgpa);
+
+        strcpy(students[i].name, name);
+        students[i].roll = roll;
+        students[i].cgpa = cgpa;
+
+    }
+    return 0;
+}
